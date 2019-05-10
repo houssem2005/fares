75a77e626106da1127b192155105953cf0ca7650
houssem2005 <46058993+houssem2005@users.noreply.github.com>
2019-05-10 18:39:00 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("https://discord.gg/mwYuzf")
+setInterval(function() {
+channel.send(`Fares manba3ou credits`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
